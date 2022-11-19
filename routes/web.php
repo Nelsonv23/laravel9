2ef89986d94ce37732e632808911e7001a0f7f8b
Nelson <nelsonv23@gmail.com>
2022-11-18 22:50:52 -0300

    Se agregan vistas blade y se modifica ruta raiz

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'welcome');
+Route::view('/nosotros', 'nosotros');
+Route::view('/servicios', 'servicios');
+Route::view('/contacto', 'contacto');
